b5e75c31ac3c463e046fb48764c0f166106c882a
CoreMaker <a845656974@outlook.com>
2026-05-24 23:59:13 +0800

    update

---
+++ STM32H503_LSM6DSV80X_P6/MDK-ARM/RTE/_STM32H503_LSM6DSV80X_P6/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32H503_LSM6DSV80X_P6' 
+ * Target:  'STM32H503_LSM6DSV80X_P6' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32h5xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
